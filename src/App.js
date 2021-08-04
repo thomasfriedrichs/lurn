94346a29f2fb5cbfa05d948414bf64dd13986aa2
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-08-04 11:59:34 -0700

    takeout movie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Movie from './hoc/Movie';
+import Counter from './components/counter';
 
 function App() {
   return (
     // <Movie id={1} />
     <div>
       <p>Hello World</p>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+const { useState, Fragment } = require("react");
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+
+  return (
+    <Fragment>
+      <input type="text" onChange={ e => setName(e.target.value)}/>
+      <p>{name}: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </Fragment>
+  )
+}
+
+export default Counter;
