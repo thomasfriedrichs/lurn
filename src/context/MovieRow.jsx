d05b68f8f599a1e30493626cf88071e9bf83a6ce
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-08-06 14:20:26 -0700

    add components

--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import UserContext from './userContext'
 
 function MoviePage(props) {
+  const currentUser = useContext(UserContext);
+  console.log("context", currentUser);
   return (
     <div>
       
