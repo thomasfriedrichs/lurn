ded3b16c8348c45db3d0c00e0e91d31e39270329
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-08-06 14:20:33 -0700

    add components

--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -4,6 +4,7 @@ import UserContext from './userContext'
 function MoviePage(props) {
   const currentUser = useContext(UserContext);
   console.log("context", currentUser);
+  
   return (
     <div>
       
