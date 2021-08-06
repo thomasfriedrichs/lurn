615aa0d1d31408e8779f9c3728e09dcd57fdb269
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-08-06 14:10:45 -0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Movie from './hoc/Movie';
-import Counter from './components/counter';
+// import Movie from './hoc/Movie';
+import Users from './hooks/Users';
 
 function App() {
   return (
     // <Movie id={1} />
     <div>
       <p>Hello World</p>
-      <Counter />
+      <Users/>
     </div>
   );
 }
