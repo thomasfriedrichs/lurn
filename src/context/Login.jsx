d9d5bb11e4bf0c888e4bb0cc906e84115d8a5dc7
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-08-06 16:27:51 -0700

    add components

--- src/context/Login.jsx
+++ src/context/Login.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext} from 'react';
+import UserContext from './userContext';
 
 function Login(props) {
+  const UserContext = useContext(UserContext);
   return (
     <div>
       <button onClick={}>Login</button>
